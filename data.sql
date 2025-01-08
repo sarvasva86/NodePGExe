21777d32fefb76632a77bfeda2b85ba579cd9570
sarvasva86 <165198643+sarvasva86@users.noreply.github.com>
2025-01-08 13:38:04 -0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -35,3 +35,19 @@ INSERT INTO invoices (comp_code, amt, paid, paid_date) VALUES
 ('apple', 100, false, null),
 ('apple', 200, true, '2023-12-01'),
 ('ibm', 300, false, null);
+
+-- Insert data into industries table
+INSERT INTO industries (code, industry) VALUES
+('acct', 'Accounting'),
+('tech', 'Technology'),
+('health', 'Healthcare'),
+('finance', 'Finance');
+
+-- Insert data into company_industries table
+INSERT INTO company_industries (industry_code, company_code) VALUES
+('acct', 'apple'),
+('tech', 'apple'),
+('tech', 'microsoft'),
+('health', 'pfizer'),
+('finance', 'goldman');
+
